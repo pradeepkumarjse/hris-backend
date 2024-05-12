6658064313e47781f6ba4d8de25b54e0c4d2e48c
pradeepkumar980 <100014151+pradeepkumar980@users.noreply.github.com>
2024-05-12 20:22:03 +0530

    first commit

--- src/main/java/com/maxaix/config/WebSecurityConfig.java
+++ src/main/java/com/maxaix/config/WebSecurityConfig.java
@@ -68,7 +68,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 				// dont authenticate this particular request
 				.authorizeRequests()
 
-                                                                  .antMatchers("/authenticate","/register","/api-doc.html", "/swagger-ui/**", "/v3/api-docs/**").permitAll().
+                                                                  .antMatchers("/authenticate","/register","/api-doc.html", "/swagger-ui/**", "/v3/api-docs/**","/api/organizations").permitAll().
 				// all other requests need to be authenticated
                                                                        
 				anyRequest().authenticated().and().
