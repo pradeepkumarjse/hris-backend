3f6faf91d9497b6e57fa52f8f30534af6acab079
pradeepkumar980 <100014151+pradeepkumar980@users.noreply.github.com>
2024-05-13 16:03:02 +0530

    first commit

--- src/main/java/com/maxaix/controller/OrganizationController.java
+++ src/main/java/com/maxaix/controller/OrganizationController.java
@@ -58,7 +58,7 @@ public class OrganizationController {
     }
 
     
-   @GetMapping("/testnew")
+   @GetMapping("/testnew_lost")
     public ResponseEntity<List<Organization>> getAllOrganizationsTest() {
         List<Organization> organizations = organizationService.getAllOrganizations();
         return new ResponseEntity<>(organizations, HttpStatus.OK);
